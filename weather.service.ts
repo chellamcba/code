0dc658bcb4fefdc4df7011190105bf97e538b1da
chellamcba <77110353+chellamcba@users.noreply.github.com>
2025-03-03 11:30:05 -0500

    Add files via upload

--- weather.service.ts
+++ weather.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map, delay } from 'rxjs/operators';
-
+/*
+This angular service file pulls data from the NOAA Weather Service. After pulling data using http get call,
+it uses the RxJS data transformation map operator to create the image. The url data is also being pulled from
+the NOAA weather REST Service. 
+*/
 @Injectable({ providedIn: 'root' })
 export class WeatherService {
   constructor(private http: HttpClient) { }
