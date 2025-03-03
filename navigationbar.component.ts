2bf7ceece5c2a56419641d821bf95cf3ff427c70
chellamcba <77110353+chellamcba@users.noreply.github.com>
2025-03-03 12:11:00 -0500

    Add files via upload

--- navigationbar.component.ts
+++ navigationbar.component.ts
@@ -1,7 +1,10 @@
 import { Component, ChangeDetectionStrategy, input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { User } from '@realworld/core/api-types';
-
+/*
+The navigation bar component takes as angular 19 feature 
+required field input "USER".
+*/
 @Component({
   selector: 'cdt-navbar',
   templateUrl: './navbar.component.html',
