a0ecfa8df838781894cbe8d618f8828904dcdc27
chellamcba <77110353+chellamcba@users.noreply.github.com>
2025-03-03 12:15:40 -0500

    Update weather.service.ts

--- weather.service.ts
+++ weather.service.ts
@@ -14,6 +14,10 @@ export class WeatherService {
   getWeatherForCityFromNoaa(city: string): Observable<any> {
     const path = `https://api.noaa.org/data/2.5/weather?q=${city}&units=metric&APPID=695ed9f29c4599b7544d0db5c211d499`;
     return this.http.get<any>(path).pipe(
+      // The spread operator (...) in JavaScript is a syntax that expands an iterable 
+      // (like an array or object) into individual elements. When used with objects, it copies the properties 
+      // from one or more objects into a new object. This allows for creating new objects based on existing ones, 
+      // adding new properties, or modifying existing ones without directly altering the original objects.  
       map(data => ({
         ...data,
         image: `http://noaa.org/img/wn/${data.weather[0].icon}@2x.png`
